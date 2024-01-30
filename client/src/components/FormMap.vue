edbc0fc5cedbf3047e7db17e21c0bb6098105737
mac <5SAJIM26@solent.ac.uk>
2024-01-30 15:40:21 +0000

    Markers are used to represent the users clicks. If they select more than 2 points the user will be informed and the third click will be taken as the new start point and the previous markers will be cleared

--- client/src/components/FormMap.vue
+++ client/src/components/FormMap.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="container">
     <div id="mapContainer"></div>
+    <button @click="clearMarkers">Clear Markers</button>
   </div>
 </template>
 
@@ -13,33 +14,46 @@ export default {
     return {
       center: [37.7749, -122.4194],
       clickCount: 0,
+      map: null,
+      markers: [],
     };
   },
   methods: {
     setupLeafletMap: function () {
-      console.log("Center:", this.center); // Log the center coordinates
+      console.log("Center:", this.center);
       const mapDiv = L.map("mapContainer").setView(this.center, 13);
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "Copyright Message"
       }).addTo(mapDiv);
-      // Add a click event listener to the map
+
       mapDiv.on("click", this.handleMapClick);
+      this.map = mapDiv;
     },
     handleMapClick: function (event) {
-      // Log the clicked coordinates
-      if (this.clickCount == 0){
-      console.log("Start Position:", event.latlng);
-      this.clickCount ++}
-      else if (this.clickCount == 1){
-      console.log("End Position:", event.latlng);
-      this.clickCount ++}
-      else{
-        alert("You have clicked more than twice. Current click will be used as start point. Now select end point")
+      if (this.clickCount === 0) {
         console.log("Start Position:", event.latlng);
-        this.clickCount = 1
+        this.markers.push(this.addMarker(event.latlng)); // Add marker at the start position and store it
+        this.clickCount++;
+      } else if (this.clickCount === 1) {
+        console.log("End Position:", event.latlng);
+        this.markers.push(this.addMarker(event.latlng)); // Add marker at the end position and store it
+        this.clickCount++;
+      } else {
+        alert("You have clicked more than twice. Current click will be used as the start point. Now select the end point.");
+        console.log("Start Position:", event.latlng);
+        this.clearMarkers();
+        this.markers.push(this.addMarker(event.latlng)); // Add marker when restarting and store it
+        this.clickCount = 1;
       }
-
-      // You can store the coordinates in your component's data or perform any other actions here
+    },
+    addMarker: function (latlng) {
+      // Create a marker and add it to the map
+      return L.marker(latlng).addTo(this.map);
+    },
+    clearMarkers: function () {
+      // Remove all markers from the map and reset the markers array
+      this.markers.forEach(marker => this.map.removeLayer(marker));
+      this.markers = [];
     },
   },
   mounted() {
