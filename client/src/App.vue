8ec5e74c915a48e3ae6591586e68102e28787025
5thaks07 <5thaks07@solent.ac.uk>
2024-02-05 14:31:01 +0000

    created a registration component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import RegistrationComponent from './components/RegistrationComponent.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <RegistrationComponent />
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
 
 <style scoped>
---
+++ client/src/components/RegistrationComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>
+      <label for="username">Username:</label>
+      <input id="username" v-model="username" />
+    </div>
+
+    <div>
+      <label for="password">Password:</label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+
+    <div>
+      <input id="checkbox" type="checkbox" v-model="checkboxChecked" />
+      <label for="checkbox">I accept the privacy policy</label>
+    </div>
+
+    <div>
+      <button id="registerbutton" @click="register">Register</button>
+    </div>
+
+    <div>
+      <a id="privacy-link" @click="openPrivacyPage">Privacy Policy</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      checkboxChecked: false,
+    };
+  },
+  methods: {
+    async register() {
+      // Simulate API call
+      if (this.username === 'existingUser') {
+        throw new Error('Username already taken');
+      }
+      // Simulate successful registration
+      // return { status: 201 };
+      this.$emit('registration-success', 'Registration successful');
+    },
+    openPrivacyPage() {
+      // Simulate opening privacy page
+      this.$emit('privacy-page-opened', 'Privacy Policy Page');
+    },
+    handleCheckboxClick() {
+      // Simulate checkbox click handling
+      this.$emit('checkbox-clicked', 'Checkbox clicked successfully');
+    },
+  },
+};
+</script>
