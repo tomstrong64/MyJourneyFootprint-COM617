d58042c0c6ae18c4d0bad83ca29279a6f0e3f769
mac <5SAJIM26@solent.ac.uk>
2024-02-05 13:53:17 +0000

    Test Commit

--- client/src/components/FormMap.vue
+++ client/src/components/FormMap.vue
@@ -11,7 +11,7 @@ export default {
   name: "Map",
   data() {
   return {
-    center: [37.7749, -122.4194],
+    center: [37.7749, -112.48746],
     clickCount: 0,
     map: null,
     markers: [],
