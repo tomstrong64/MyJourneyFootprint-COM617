b03f57d3a635bcccaad4c6cce8e8b53489fd4669
5thaks07 <5thaks07@solent.ac.uk>
2024-02-28 21:02:27 +0000

    Added some e2e tests for index page

---
+++ client/cypress/e2e/index.cy.js
@@ -0,0 +1,26 @@
+describe('Index Page Tests', () => {
+  beforeEach(() => {
+    // Visit the page before each test
+    cy.visit('/')
+  })
+
+  it('Renders without errors', () => {
+    cy.get('[data-cy="header-nav-bar"]').should('exist')
+    cy.get('.bg-gray-900').should('exist')
+    cy.get('[data-cy="get-started-link"]').should('exist')
+    cy.get('[data-cy="sign-up-link"]').should('exist')
+    cy.get('[data-cy="footer-component"]').should('exist')
+  })
+
+  it('Navigates to the Calculate page when "Get started" is clicked', () => {
+    cy.get('[data-cy="get-started-link"]').click()
+    cy.url().should('include', '/calculate')
+  })
+
+  it('Navigates to the Register page when "Sign Up" is clicked', () => {
+    cy.get('[data-cy="sign-up-link"]').click()
+    cy.url().should('include', '/register')
+  })
+
+  // Add more test cases as needed
+})
